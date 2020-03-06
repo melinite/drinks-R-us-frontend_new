b03b9c4f019fa51838c96b626cdb8ae9b7242b73
melinite <melinite@gmail.com>
2020-03-06 08:05:06 -0500

    feature(users): start of new Users component

---
+++ src/pages/Users.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import AuthService from '../components/AuthService';
+import withAuth from '../components/withAuth';
+import '../App.css';
+const Auth = new AuthService();
+
+class Users extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mt-5 mx-auto">
+                        IF YOU SEE THIS YOU ARE LOGGED IN :)
+                        <br/>
+                       Add Fetch User component Here
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default withAuth(Users);
