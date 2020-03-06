310d8c2f6690b0afeb366550f0a74cdc6476a7ec
melinite <melinite@gmail.com>
2020-03-06 09:21:11 -0500

    feature(Profile) - update profile to retrieve user data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,60 +1,79 @@
 import React, { Component } from 'react';
-//import { getProfileData } from '../api/userApi';
+import AuthService from '../components/AuthService';
+import withAuth from '../components/withAuth';
+import { getProfileData } from '../api/userApi';
 import '../App.css';
 
 class Profile extends Component {
-   constructor() {
-       super();
-       this.state = {
-           loaded: false,
-           f_name: '',
-           l_name: '',
-           email: '',
-           phone:'',
-           errors: {},
-       };
-   }
+    constructor() {
+        super();
+        this.state = {
+            loaded: false,
+            f_name: '',
+            l_name: '',
+            email: '',
+            phone: '',
+            errors: {},
+        };
+    }
 
-   // async componentDidMount() {
-   //     this.setState({ loaded: false });
-   //     const profileData = await getProfileData();
-   //     this.setState({
-   //         loaded: true,
-   //         ...profileData,
-   //     });
-   // }
+    componentDidMount() {
+        getProfileData()
+            .then(profileData => {
+                this.setState({
+                    loaded: true,
+                    ...profileData,
+                });
+            });
+    }
 
-
-   render() {
-       if (!this.state.loaded) {
-           return <div>Loading</div>;
-       }
-       return (
-           <div className="container">
-               <div className="jumbotron mt-5">
-                   <div className="col-sm-8 mx-auto">
-                       <h1 className="text-center">PROFILE</h1>
-                   </div>
-                   <table className="table col-md-6 mx-auto">
-                       <tbody>
-                       <tr>
-                           <td>Fist Name</td>
-                           <td>{this.state.first_name}</td>
-                       </tr>
-                       <tr>
-                           <td>Last Name</td>
-                           <td>{this.state.last_name}</td>
-                       </tr>
-                       <tr>
-                           <td>Email</td>
-                           <td>{this.state.email}</td>
-                       </tr>
-                       </tbody>
-                   </table>
-               </div>
-           </div>
-       );
-   }
+    render() {
+        if (!this.state.loaded) {
+            return <div>Loading</div>;
+        }
+        return (
+            <div className="container">
+                <div className="jumbotron mt-5">
+                    <div className="col-sm-4 mx-auto">
+                        <h1 className="text-center">PROFILE</h1>
+                    </div>
+                    <table className="table col-md-4">
+                        <tbody>
+                            <tr>
+                                <td style={{ "text-align": "left" }}><div class="form-group">
+                                    <label for="inputEmail">First Name</label>
+                                    <input type="email" class="form-control" id="inputEmail" placeholder="Email" value={this.state.f_name} />
+                                </div>
+                                </td>
+                        
+                            </tr>
+                            <tr>
+                            <td style={{ "text-align": "left" }}><div class="form-group">
+                                    <label for="inputEmail">Last Name</label>
+                                    <input type="email" class="form-control" id="inputEmail" placeholder="Email" value={this.state.l_name} />
+                                </div>
+                                </td>
+                            </tr>
+                            <tr>
+                            <td style={{ "text-align": "left" }}><div class="form-group">
+                                    <label for="inputEmail">Email</label>
+                                    <input type="email" class="form-control" id="inputEmail" placeholder="Email" value={this.state.email} />
+                                </div>
+                                </td>
+                            </tr>
+                            <tr>
+                            <td style={{ "text-align": "left" }}><div class="form-group">
+                                    <label for="inputEmail">Phone</label>
+                                    <input type="email" class="form-control" id="inputEmail" placeholder="Email" value={this.state.phone} />
+                                </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default Profile;
+export default withAuth(Profile);
