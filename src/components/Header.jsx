6a58d1721872e3c3615501aae6a1a9ee26be447c
melinite <melinite@gmail.com>
2020-03-06 08:03:04 -0500

    feature(header): updates header component - adds logout button, Users Route,

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,78 @@
 import '../App.css';
-import React from 'react';
-import Cart from  '../pages/Cart';
-import Beer from  '../pages/Beer';
-import Liquors from  '../pages/Liquors';
-import Login from  '../pages/Login';
-import Profile from  '../pages/Profile';
-import Register from  '../pages/Register';
-import Admin from  '../pages/Admin';
-import Wine from  '../pages/Wine';
+import React, { Component } from 'react'
+import Cart from '../pages/Cart';
+import Beer from '../pages/Beer';
+import Liquors from '../pages/Liquors';
+import Login from '../pages/Login';
+import Profile from '../pages/Profile';
+import Register from '../pages/Register';
+import Admin from '../pages/Admin';
+import Wine from '../pages/Wine';
+import Users from '../pages/Users';
 import { NavLink, Switch, Route } from 'react-router-dom';
+import AuthService from './AuthService';
+import { withRouter } from "react-router";
 
-class Header extends React.Component {
-
+class Header extends Component {
+    constructor() {
+        super();
+        this.Auth = new AuthService();
+        this.logout = this.logout.bind(this)
+    }
+    logout() {
+        this.Auth.logout();
+        alert('logged out!');
+        this.props.history.replace('/Login');
+    }
     render() {
 
         /*Need to relook at this part for quantity*/
         let unitTotal = 0;
 
-        const totalCartQty = this.props.check.cart.forEach( (cart, index) => 
-        {
-          unitTotal += cart.units;
-          return unitTotal;
+        const totalCartQty = this.props.check.cart.forEach((cart, index) => {
+            unitTotal += cart.units;
+            return unitTotal;
         });
 
-       return (
-          <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <NavLink to="/" className="navbar-brand">DRINKS-R-US</NavLink>
-                <NavLink to="/" className="nav-link text-light"> Home </NavLink>
-                <NavLink to="/Liquors" className="nav-link text-light"> Liquors </NavLink>
-                <NavLink to="/Beer" className="nav-link text-light"> Beer </NavLink>
-                <NavLink to="/Wine" className="nav-link text-light"> Wine </NavLink>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink to={{pathname: "/Cart", state: this.props.check}} className="nav-link"> Cart ({unitTotal})</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/Register" className="nav-link"> Register </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/Login" className="nav-link"> Login </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/Profile" className="nav-link"> Profile </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/Admin" className="nav-link"> Admin </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <NavLink to="/" className="navbar-brand">DRINKS-R-US</NavLink>
+                    <NavLink to="/" className="nav-link text-light"> Home </NavLink>
+                    <NavLink to="/Liquors" className="nav-link text-light"> Liquors </NavLink>
+                    <NavLink to="/Beer" className="nav-link text-light"> Beer </NavLink>
+                    <NavLink to="/Wine" className="nav-link text-light"> Wine </NavLink>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink to={{ pathname: "/Cart", state: this.props.check }} className="nav-link"> Cart ({unitTotal})</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/Register" className="nav-link"> Register </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/Login" className="nav-link"> Login </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/Profile" className="nav-link"> Profile </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/Admin" className="nav-link"> Admin </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/Users" className="nav-link"> Users </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" onClick={this.logout}> Logout </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
 
-               <Switch>
+                <Switch>
                     <Route path="/Beer" component={Beer} />
                     <Route path="/Cart" render={(props) => <Cart cartData={this.props.check} />} />
                     <Route path="/Liquors" component={Liquors} />
@@ -64,10 +81,11 @@ class Header extends React.Component {
                     <Route path="/Admin" component={Admin} />
                     <Route path="/Register" component={Register} />
                     <Route path="/Wine" component={Wine} />
-               </Switch>
-          </div>
-       );
+                    <Route path="/Users" component={Users} />
+                </Switch>
+            </div>
+        );
     }
-  }
+}
 
-  export default Header;
+export default withRouter(Header);
